6c2e4e46287a732833bfb94496c862e8326a4601
unknown <mandisazina818@gmail.com>
2023-06-05 15:04:45 +0200

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite';
+import data from './data.json';
+
+   export default defineConfig({
+        base : '/responsive-webpages/',
+        build: {
+          rollupOptions: {
+              input : {
+                  main: 'index.html',
+                  bootstrap: 'index-bootstrap.html',
+                  materialize: 'index-materialize.html',
+              }
+          },
+      },
+      plugins: [handlebars({
+        context : {
+          data
+        }
+    })]
+      
+  });
